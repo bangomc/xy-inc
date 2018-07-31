68e7cdcccc56f181a18021b05e1970c8fd47d730
Cristhiano Roberto <bangomc@gmail.com>
2018-07-30 23:25:31 -0300

    Criacao de tabela

--- src/main/java/com/xyinc/controller/TabelaController.java
+++ src/main/java/com/xyinc/controller/TabelaController.java
@@ -3,20 +3,22 @@ package com.xyinc.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.xyinc.bean.Coluna;
 import com.xyinc.bean.Tabela;
+import com.xyinc.bean.enums.TipoDado;
 import com.xyinc.repository.TabelaRepository;
 import com.xyinc.service.DatabaseService;
 
-@RestController
+@Controller
 @RequestMapping("/tabela")
 public class TabelaController {
 	
@@ -38,23 +40,39 @@ public class TabelaController {
 		
 		if(tabela.isPresent()) {
 			mv.addObject("tabela",tabela.get());
+			mv.addObject("tiposDado",TipoDado.values());
 		}
 		
 		return mv;
 	}
 	
-	public ModelAndView addColuna(Coluna coluna) {
+	
+	@PostMapping(params="adicionarColuna")
+	public ModelAndView addColuna(Tabela tabela) {
 		ModelAndView mv = new ModelAndView("tabela/inc");
-		
+		tabela.getColunas().add(new Coluna());
+		mv.addObject("tabela",tabela);
+		mv.addObject("tiposDado",TipoDado.values());
+		return mv;
+	}
+	
+	@PostMapping(params="removerColuna")
+	public ModelAndView delColuna(Tabela tabela, HttpServletRequest req) {
+		ModelAndView mv = new ModelAndView("tabela/inc");
+		Integer index = Integer.valueOf(req.getParameter("removerColuna"));		
+		tabela.getColunas().remove(index.intValue());
+		mv.addObject("tabela",tabela);
+		mv.addObject("tiposDado",TipoDado.values());
 		return mv;
 	}
 	
 	@PostMapping
-	public Tabela inc(@RequestBody Tabela tabela) {
-		Tabela retorno = tabelaRepository.save(tabela);
-		databaseService.criarTabela(tabela);
-		return retorno;
+	public Tabela inc(Tabela tabela) {
+		//Tabela retorno = tabelaRepository.save(tabela);
+		//databaseService.criarTabela(tabela);
+		return tabela;
 	}
+		
 
 	public TabelaRepository getTabelaRepository() {
 		return tabelaRepository;
